f2b4f30c0c2694e583d29c36a36fdf730d2ac62d
Devashish Rana <devashish@localhost.localdomain>
2016-09-10 20:16:33 +0530

    Using objects and checking This in JS

---
+++ this.js
@@ -0,0 +1,26 @@
+//This - refers to the object which calls it
+
+var Dev = {
+    printFirstName: function() {
+        console.log("my name is devashish");
+        console.log(this === Dev);
+    }
+};
+
+Dev.printFirstName();
+
+
+
+
+//the default calling context is global
+
+function doSomething() {
+    console.log("my name is devashish");
+    //checking for object Dev
+    console.log(this === Dev);
+    
+    //Checking for global
+    console.log(this === global);
+}
+
+doSomething();
